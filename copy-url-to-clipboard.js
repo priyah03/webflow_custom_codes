9180d254eba0e39b777da5f67d8d17fc321f1e26
PlayerParticle <balrutuparna@gmail.com>
2022-08-16 02:11:19 +0530

    Create copy-url-to-clipboard.js
    Add the script before footer/body section.

---
+++ copy-url-to-clipboard.js
@@ -0,0 +1,12 @@
+//Create a div/button/link block and give it a classname ‘clipboard’.
+<script>
+var $temp = $("<input>");
+var $url = $(location).attr('href');
+
+$('.clipboard').on('click', function() {
+  $("body").append($temp);
+  $temp.val($url).select();
+  document.execCommand("copy");
+  $temp.remove();
+})
+</script>
